4f255c6c18ca0a4d87a5b1326c0d956578fafa8b
Ramesh Rabari <ramesh.r@ainfosolutions.com>
2025-09-16 01:20:10 +0530

    Relieving Letter - Printing

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -63,7 +63,7 @@
 					</tr>
 					<tr>
 						<th class="border p-2 text-left">Relieving Letter</th>
-						<td class="border p-2"><a href="{{ route('employees.relieving-letter', $employee->id) }}"><button class="bg-blue-500 text-white px-4 py-2 rounded">Print</button></a></td>
+						<td class="border p-2"><a target="_blank" href="{{ route('employees.relieving-letter', $employee->id) }}"><button class="bg-blue-500 text-white px-4 py-2 rounded">Print</button></a></td>
 					</tr>
                 </table>
 
